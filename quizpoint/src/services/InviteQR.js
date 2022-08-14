a5457576925a540ea56a76956d88c8a1c062d5f3
Max Webb <maxwebblighting@gmail.com>
2022-08-14 17:15:48 +1200

    QR Code PDF exports avaliable.

--- quizpoint/src/services/InviteQR.js
+++ quizpoint/src/services/InviteQR.js
@@ -1,5 +1,5 @@
 import Button from "@mui/material/Button";
-import { useState, React } from "react";
+import { useState, React, useRef } from "react";
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import DialogActions from '@mui/material/DialogActions';
@@ -12,12 +12,32 @@ import Slide from '@mui/material/Slide';
 import Dialog from '@mui/material/Dialog';
 import logo from '../main/components/logo.svg'
 import { QRCode } from 'react-qrcode-logo';
+import jsPDF from "jspdf";
 
 import './InviteQR.css'
 export default function InviteQR(props) {
+
     console.log(props)
     let inviteLink = `https://quizpointnz.netlify.app/invite/${props.classObject.code}`
     const [openDialog, setDialog] = useState(false)
+
+    function downloadPDF() {
+        // Defines the pdf
+        let pdf = new jsPDF({
+        })
+        pdf.setFontSize(25);
+        pdf.text(`Scan to join ${props.classObject.className} on QuizPoint`, 40, 30);
+
+        // Transforms the canvas into a base64 image
+        let base64Image = document.getElementById('qrcode').toDataURL()
+
+        // Adds the image to the pdf
+        pdf.addImage(base64Image, 'png', 5, 40, 200, 200)
+
+        // Downloads the pdf
+        pdf.save(`QR code to join ${props.classObject.className}.pdf`)
+    }
+
     function QRInvite() {
         return (
             <Dialog
@@ -42,7 +62,7 @@ export default function InviteQR(props) {
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                <div className='inviteDialog'>
+                <div className='inviteDialog' >
                     <div className="inviteContent">
                         <div className="inviteText">
                             <h2>Join {props.classObject.className} on QuizPoint</h2>
@@ -51,6 +71,7 @@ export default function InviteQR(props) {
                         <div className="inviteQR">
                             <QRCode
                                 logoImage={logo}
+                                id='qrcode'
                                 logoWidth={100}
                                 logoHeight={100}
                                 size={500}
@@ -60,6 +81,7 @@ export default function InviteQR(props) {
                         </div>
 
                     </div>
+                    <Button onClick={() => downloadPDF()}>Create a QR Code PDF</Button>
 
                 </div>
             </Dialog>
