6bfc82b8ec99450210b7246fff09b0a0c31191c1
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-08-23 11:54:43 +1200

    Login feature added to navigation, re-styling reauthentication pages to match new login.

--- quizpoint/src/components/navbar/CreateNavbar.js
+++ quizpoint/src/components/navbar/CreateNavbar.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022 Bounce developed by alanmcilwaine and maxwebbnz
+ * All rights reserved.
+ */
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import { user } from '../firebase/fb.user';
@@ -6,7 +10,7 @@ import Logo from '../../media/logo.svg'
 
 const CreateNavbar = (props) => {
     let navigation;
-    switch(props.role) {
+    switch (props.role) {
         // Changes the navigation depending on the role of the user
         case 'teacherHome':
             navigation = [
@@ -21,7 +25,7 @@ const CreateNavbar = (props) => {
             break;
         case 'teachingSuite':
             navigation = [
-                { name: 'Students', href: '/tcs/students/all', current: true },
+                { name: 'Students', href: '/tcs/students/all', current: false },
                 { name: 'Teachers', href: '/tcs/teachers/all', current: false },
                 { name: 'Classes', href: '/tcs/classes', current: false },
                 { name: 'Quizzes', href: '/tcs/quizzes', current: false },
@@ -48,131 +52,131 @@ const CreateNavbar = (props) => {
         <Disclosure as="nav" className="bg-primary-100 ">
             {({ open }) => (
                 <>
-                <div className="mx-auto px-3 py-2 sm:px-6 lg:px-8 max-w-[85%]">
-                    <div className="relative flex items-center justify-between h-16">
-                        {/* Mobile Navigation Button */}
-                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                            <span className="sr-only">Open main menu</span>
-                            {/* Change Icon If Dropdown is Opened */}
-                            {open ? (
-                                <XIcon className="block h-6 w-6" aria-hidden="true" />
-                            ) : (
-                                <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                            )}
-                            </Disclosure.Button>
-                        </div>
+                    <div className="mx-auto px-3 py-2 sm:px-6 lg:px-8 max-w-[85%]">
+                        <div className="relative flex items-center justify-between h-16">
+                            {/* Mobile Navigation Button */}
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="sr-only">Open main menu</span>
+                                    {/* Change Icon If Dropdown is Opened */}
+                                    {open ? (
+                                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
 
-                        <div className=" items-start hidden sm:block">
-                            <a href="/classes"><img className="w-auto h-12" src={Logo} alt="Logo"/></a>
-                        </div>
+                            <div className=" items-start hidden sm:block">
+                                <a href="/classes"><img className="w-auto h-12" src={Logo} alt="Logo" /></a>
+                            </div>
 
-                        {/* Main Desktop Content */}
-                        <div className="flex-1 flex items-center content-center justify-center sm:items-stretch">
-                            {/* Logo */}
-                            <div className="hidden sm:block">
-                                <div className="flex space-x-4 border-slate-300 border p-2 rounded-lg shadow-md">
-                                    {/* Generate a new navigation button for each in array navigation */}
-                                    {navigation.map((item) => (
-                                    <a
-                                        onClick = {() => updateCurrentInNavigationArray(item)}
-                                        key={item.name}
-                                        href={item.href}
-                                        className={classNames(
-                                        item.current ? 'font-bold' : 'font-medium',
-                                        'px-4 py-2 text-sm m-0 last:mr-0 border-r last:border-0 text-white relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[1px] after:w-full after:translate-y-1 after:opacity-0 hover:after:translate-y-0 hover:after:opacity-100 after:transition after:ease-in-out after:duration-200',
-                                        )}
-                                    >
-                                        {item.name}
-                                    </a>
-                                    ))}
+                            {/* Main Desktop Content */}
+                            <div className="flex-1 flex items-center content-center justify-center sm:items-stretch">
+                                {/* Logo */}
+                                <div className="hidden sm:block">
+                                    <div className="flex space-x-4 border-slate-300 border p-2 rounded-lg shadow-md">
+                                        {/* Generate a new navigation button for each in array navigation */}
+                                        {navigation.map((item) => (
+                                            <a
+                                                onClick={() => updateCurrentInNavigationArray(item)}
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'font-bold' : 'font-medium',
+                                                    'px-4 py-2 text-sm m-0 last:mr-0 border-r last:border-0 text-white relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[1px] after:w-full after:translate-y-1 after:opacity-0 hover:after:translate-y-0 hover:after:opacity-100 after:transition after:ease-in-out after:duration-200',
+                                                )}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
 
 
-                        {/* Desktop Profile dropdown */}
-                        <div className="flex items-end">
-                            <Menu as="div" className="relative">
-                            <div className="flex align-center ">
-                                <Menu.Button className="p-0">
-                                <span className="sr-only">Open user menu</span>
-                                <img className="w-12 h-12 rounded-lg" src={user.picture} alt="Profile Picture"/>
-                                </Menu.Button>
+                            {/* Desktop Profile dropdown */}
+                            <div className="flex items-end">
+                                <Menu as="div" className="relative">
+                                    <div className="flex align-center ">
+                                        <Menu.Button className="p-0">
+                                            <span className="sr-only">Open user menu</span>
+                                            <img className="w-12 h-12 rounded-lg" src={user.picture} alt="Profile Picture" />
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                    >
+                                                        Your Profile
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                    >
+                                                        Settings
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <a
+                                                        href="/logout"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                    >
+                                                        Sign out
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                        </Menu.Items>
+                                    </Transition>
+                                </Menu>
                             </div>
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95"
-                            >
-                                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                <Menu.Item>
-                                    {({ active }) => (
-                                    <a
-                                        href="#"
-                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                    >
-                                        Your Profile
-                                    </a>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                    <a
-                                        href="#"
-                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                    >
-                                        Settings
-                                    </a>
-                                    )}
-                                </Menu.Item>
-                                <Menu.Item>
-                                    {({ active }) => (
-                                    <a
-                                        href="#"
-                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                    >
-                                        Sign out
-                                    </a>
-                                    )}
-                                </Menu.Item>
-                                </Menu.Items>
-                            </Transition>
-                            </Menu>
                         </div>
                     </div>
-                </div>
 
-                <Disclosure.Panel className="sm:hidden">
-                    <div className="px-4 pt-2 pb-3 space-y-1">
-                    {navigation.map((item) => (
-                        <Disclosure.Button
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="px-4 pt-2 pb-3 space-y-1">
+                            {navigation.map((item) => (
+                                <Disclosure.Button
 
-                        onClick = {() => updateCurrentInNavigationArray()}
+                                    onClick={() => updateCurrentInNavigationArray()}
 
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className={classNames(
-                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'block px-3 py-2 rounded-md text-base font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                        >
-                        {item.name}
-                        </Disclosure.Button>
-                    ))}
-                    </div>
-                </Disclosure.Panel>
+                                    key={item.name}
+                                    as="a"
+                                    href={item.href}
+                                    className={classNames(
+                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'block px-3 py-2 rounded-md text-base font-medium'
+                                    )}
+                                    aria-current={item.current ? 'page' : undefined}
+                                >
+                                    {item.name}
+                                </Disclosure.Button>
+                            ))}
+                        </div>
+                    </Disclosure.Panel>
                 </>
             )}
         </Disclosure>
-     );
+    );
 }
- 
+
 export default CreateNavbar;
--- quizpoint/src/teachingsuite/ReAuthTeacher.js
+++ quizpoint/src/teachingsuite/ReAuthTeacher.js
@@ -46,19 +46,21 @@ export default function ReAuthenticateTeacher() {
     }
 
     return (
-        <div id="landingPage" className="loginPage" style={{ backgroundImage: `url(${schoolMedia})` }}>
-            <div className="loginContainer">
-                <div className="logo"><img src={logo} alt="QuizPoint Logo"></img></div>
-                <div className="loginText">
-                    <h3>Please re-authenticate</h3>
-                    <h5>We need to check your identity</h5>
+        <div className="flex flex-row w-screen h-screen overflow-y-hidden">
+            <div className="md:order-1 md:basis-2/3 bg-blue-200 flex w-screen flex-col justify-center items-center">
+                <div className="md:h-2/5 md:w-2/3 h-3/5 w-4/5 flex gap-10 flex-column justify-center">
+                    <div>
+                        <p className="text-start font-medium text-3xl text-black py-2">We need to confirm your identity.</p>
+                        <p className="w-full">Have a problem with this? <a href="mailto:katie.long@hvhs.school.nz" className="underline underline-offset-8" alt="Support email">Email us for support</a></p>
+                    </div>
+                    <button className="flex hover:shadow-lg md:w-96 max-w-lg" onClick={() => login()}><img className="w-8 m-0 h-fit inline" src={googleButton} alt="Login Button" /><p className="flex justify-center items-center w-full h-8">Sign In With Google</p></button>
                 </div>
-                <div className="loginButtons">
-
-                    <Button variant="light" size="lg" className="googleLoginButton" onClick={() => login()} id="authButton"><img src={googleButton}></img>Sign In with Google</Button>
-                </div>
-
             </div>
-
+            <div className="md:order-2 md:basis-1/3 bg-red-300 flex-col hidden md:block">
+                <div className="flex flex-row items-center w-full h-full">
+                    <img className="w-20 h-20 px-1" src={logo} alt="logo" />
+                    <div className="text-4xl font-medium relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300">QuizPoint</div>
+                </div>
+            </div>
         </div>)
 }
