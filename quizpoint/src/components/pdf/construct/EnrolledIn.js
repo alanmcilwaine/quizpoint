b9450348bf853b1dbcb235c25e36222faf1d7237
MJW <60947369+maxwebbnz@users.noreply.github.com>
2022-08-25 12:46:51 +1200

    Staging changes.

---
+++ quizpoint/src/components/pdf/construct/EnrolledIn.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 Bounce developed by alanmcilwaine and maxwebbnz
+ * All rights reserved.
+ */
+export default function EnrolledIn({ student }) {
+    return (
+        <p className="text-sm font-medium leading-tight text-gray-500">
+            User is enrolled in
+        </p>
+    )
+}
